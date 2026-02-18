098e885863e2673287275029c314b761c7e1a2ff
Aryan Sachan <sachanaryaniit@gmail.com>
2026-02-19 00:43:41 +0530

    correct the async function of health check

--- backend/src/controllers/apiHealthcheck.controller.js
+++ backend/src/controllers/apiHealthcheck.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/AsyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 
-const apiHealth = asyncHandler(async (res) => {
+const apiHealth = asyncHandler(async (_, res) => {
     res.status(200).json(
         new ApiResponse(200, {}, "Trello API is healthy and running")
     )
